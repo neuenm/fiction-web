522ec7a2175ec26355d31313b491b0d25d3d6955
neuenm <neuenmobilia@gmail.com>
2024-12-21 10:55:21 -0300

    Refactor LoginForm to use requestLogin prop for authentication; add error handling with FormErrorMessage and toast notifications

--- app/(noAuth)/login/client/form.jsx
+++ app/(noAuth)/login/client/form.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import { Input } from '@/components/ui/input';
 import { LoadingButton } from '@/components/ui/loadingButton';
 import { Form } from '@/components/ui/form';
@@ -8,47 +8,45 @@ import * as yup from 'yup';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useRouter } from 'next/navigation';
-import fetchWrapperServer from '@/lib/fetchWrapperServer';
+import { useToast } from '@/hooks/use-toast';
+import FormErrorMessage from '@/components/ui/formErrorMessage';
 
-export default function LoginForm() {
-  const [loading, setLoading] = useState(false);
+export default function LoginForm({ requestLogin }) {
   const router = useRouter();
   const schemaLogin = yup.object({
     email: yup.string().email('Formato invalido').required('Requerido'),
     password: yup.string().required('Requerido'),
   });
 
+  const { toast } = useToast();
+
   const form = useForm({
-    mode: 'onChange',
+    mode: 'onSubmit',
     resolver: yupResolver(schemaLogin),
   });
 
   const {
-    formState: { isSubmitting, isDirty, isValid },
+    formState: { isSubmitting, isValid, errors },
     register,
     handleSubmit,
   } = form;
 
   const handleLogin = async (values) => {
     try {
-      const data = await fetchWrapperServer({
-        url: 'login',
-        method: 'POST',
-        body: {
-          email: values.email,
-          password: values.password,
-        },
-      });
-
+      const data = await requestLogin(values);
       if (data.accessToken) {
-        document.cookie = `token=${data.accessToken}; path=/; SameSite=Strict`;
+        document.cookie = `token=${data.accessToken}; path=/; SameSite=Strict; max-age=${30 * 60}`; //Expires in 30 minutes (the token is valid for 20 minutes in json-server)
         document.cookie = `user=${encodeURIComponent(
           JSON.stringify(data.user)
         )}; path=/; SameSite=Strict`;
         router.push('/books');
       }
     } catch (error) {
-      console.error('Error:', error);
+      toast({
+        variant: 'destructive',
+        title: 'Algo salio mal',
+        description: error.message,
+      });
     }
   };
 
@@ -58,7 +56,11 @@ export default function LoginForm() {
         <form onSubmit={handleSubmit(handleLogin)}>
           <div className='flex flex-col space-y-4'>
             <Input {...register('email')} placeholder='Email' id='email' type='email' />
+            {errors.email && <FormErrorMessage error={errors.email.message} />}
+
             <Input {...register('password')} placeholder='Password' id='password' type='password' />
+            {errors.password && <FormErrorMessage error={errors.password.message} />}
+
             <LoadingButton
               type='submit'
               variant='default'
--- app/(noAuth)/login/page.jsx
+++ app/(noAuth)/login/page.jsx
@@ -4,15 +4,30 @@ import Image from 'next/image';
 import { logo } from '@Public/images';
 import { Col } from '@/components/col';
 import { Row } from '@/components/row';
+import fetchWrapperServer from '@/lib/fetchWrapperServer';
 
 export default function page() {
+  const requestLogin = async (values) => {
+    'use server';
+    const data = await fetchWrapperServer({
+      url: 'login',
+      method: 'POST',
+      body: {
+        email: values.email,
+        password: values.password,
+      },
+    });
+
+    return data;
+  };
+
   return (
     <Row className='h-screen !gap-0'>
       <Col xs={12} md={6} className='flex items-center justify-center '>
         <Image src={logo} alt='Example' width={300} height={300} className='object-contain' />
       </Col>
       <Col xs={12} md={6} className='flex items-center justify-center md:h-screen h-52'>
-        <LoginForm />
+        <LoginForm requestLogin={requestLogin} />
       </Col>
     </Row>
   );
