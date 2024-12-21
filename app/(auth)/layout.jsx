69c2a51bc37e8e448e068e85d8269b5a90bdf555
neuenm <neuenmobilia@gmail.com>
2024-12-21 10:56:44 -0300

    Enhance AuthLayout to include token validation and improve layout structure for better user experience

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -11,15 +11,18 @@ export default async function AuthLayout({ children }) {
     const userString = decodeURIComponent(user.value);
     userObject = JSON.parse(userString);
   }
-  //if the user is not loged in, redirect to the login page
-  if (!token) {
+
+  // if the user is not loged in, redirect to the login page
+  if (!token || !token.value) {
     redirect('/login');
   }
 
   return (
     <>
       <Header user={userObject} />
-      <div className='container p-10'>{children}</div>
+      <div className='flex justify-center align-center bg-primary-100 min-h-[calc(100vh-5rem)]   '>
+        <div className='container'>{children}</div>
+      </div>
     </>
   );
 }
