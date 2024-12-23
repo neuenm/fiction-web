96b3918c62d74869d7f543bf1d1277facc4a208a
neuenm <neuenmobilia@gmail.com>
2024-12-22 23:59:28 -0300

    Update webpack aliases to use process.cwd() for improved path resolution

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
-/** @type {import('next').NextConfig} */
 import path from 'path';
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['tienda.planetadelibros.com.ar', 'www.tematika.com', 'www.lapatilla.com'],
   },
   webpack: (config) => {
-    config.resolve.alias['@Public'] = path.resolve(__dirname, 'public');
-    config.resolve.alias['@Utils'] = path.resolve(__dirname, 'utils');
+    config.resolve.alias['@Public'] = path.resolve(process.cwd(), 'public');
+    config.resolve.alias['@Utils'] = path.resolve(process.cwd(), 'utils');
 
     return config;
   },
